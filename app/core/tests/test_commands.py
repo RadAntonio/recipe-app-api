07b3bb4a4dd060685240382a4f754c8afa1c4a32
Rad Antonio <tonirad@icloud.com>
2025-02-19 02:28:27 +0200

    Create user model

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,4 +1,4 @@
-"""TEst custom Django managememnt commands."""
+"""Test custom Django managememnt commands."""
 from unittest.mock import patch
 
 from psycopg2 import OperationalError as Psycopg2Error
